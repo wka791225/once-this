eee4af45505ed9dccc01be78996d8c8ca7f5b808
wka791225 <93628110+wka791225@users.noreply.github.com>
2022-07-04 18:24:12 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NewController;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Route;
@@ -18,8 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/user', [App\Http\Controllers\HomeController::class, 'user']);
+Route::get('/user', [HomeController::class, 'user']);
 Route::get('/index',[NewController::class,'index']);
 Auth::routes();
 
